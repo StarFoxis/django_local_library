57ea1f8e7c05ecab961299ada97601fa96f38679
StarFoxis <milovanovv1@mail.ru>
2022-04-20 11:17:20 +0500

    Settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -144,5 +144,5 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
